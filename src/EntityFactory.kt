9873885c732bdc601451e1fc0d002e82e0e534a0
albae69 <ahmadalbaihaqi69@gmail.com>
2021-02-02 21:06:46 +0700

    enum classes

--- src/EntityFactory.kt
+++ src/EntityFactory.kt
@@ -1,22 +1,33 @@
-interface IdProvider {
-    fun getId(): String
+import java.util.*
+
+enum class EntityType {
+    EASY, MEDIUM, HARD;
+
+    fun getFormattedName() = name.toLowerCase().capitalize()
+}
+
+object EntityFactory {
+    fun create(type: EntityType): Entity {
+        val id = UUID.randomUUID().toString()
+        val name = when (type) {
+            EntityType.EASY -> type.name
+            EntityType.MEDIUM -> type.getFormattedName()
+            EntityType.HARD -> "Hard"
+        }
+        return Entity(id,name)
+    }
 
 }
 
-class Entity private constructor(val id: String) {
-
-    companion object Factory : IdProvider {
-        override fun getId(): String {
-            return "123"
-        }
-
-        const val id = "id"
-        fun create() = Entity(getId())
+class Entity(val id: String,val name:String) {
+    override fun toString(): String {
+        return "id: $id name: $name"
     }
 }
 
 fun main() {
-
-    val entity = Entity.Factory.create()
-    Entity.id
+    val easyEntity = EntityFactory.create(EntityType.EASY)
+    val mediumEntity = EntityFactory.create(EntityType.MEDIUM)
+    println(easyEntity)
+    println(mediumEntity)
 }
