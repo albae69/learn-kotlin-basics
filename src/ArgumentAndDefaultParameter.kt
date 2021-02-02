196753107a17a8be661d605d2c3c192dd5060efb
albae69 <ahmadalbaihaqi69@gmail.com>
2021-02-02 09:11:46 +0700

    vararg

---
+++ src/ArgumentAndDefaultParameter.kt
@@ -0,0 +1,15 @@
+//var arg parameter -> pass an value after initialized
+fun sayHello(greeting: String, vararg itemsToGreet: String) {
+    itemsToGreet.forEach { items ->
+        println("$greeting $items")
+    }
+}
+
+fun main() {
+//    sayHello("Hi", "Kotlin", "Programming", "Cat") //vararg treated as list of argument
+//    instead hardcode the array, expect -> get an array data from api, you can pass an array into vararg parameter
+//    but you should add a spread operator ( * ) , if not the program will throw an error
+    val interestingThings = arrayOf("Kotlin", "Programming", "Cat")
+    sayHello("Hi", *interestingThings) //vararg treated as list of argument
+
+}
